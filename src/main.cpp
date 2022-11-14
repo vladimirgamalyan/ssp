0f3c6ad686cc143f4f20df1e621d7b1605aeffb0
Vladimir Gamalyan <vladimir.gamalyan@gmail.com>
2022-11-14 13:07:24 +0700

    Add doctest testing framework

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
+#define DOCTEST_CONFIG_IMPLEMENT
+#include "external/doctest.h"
+
+TEST_CASE("foo test") {
+    CHECK(2 * 2 == 4);
+}
+
 int main(int argc, char* argv[])
 {
-    std::cout << "Hello World!\n";
+	doctest::Context context(argc, argv);
+	int res = context.run();
+	if (context.shouldExit() || res)
+		return res;
 }
